d8854fa30402dc904eb5e5ee0d70fc4c9e93ca62
c-salhab <charbel.salhab.2003@gmail.com>
2024-02-09 10:28:51 +0100

    upload json  files done + testing done

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from outils import upload_file
 
 if __name__ == "__main__":
     datas = upload_file.upload_file("C:\\Users\\csalhab\\OneDrive\\Online Sessions\\3iabd1\\Scripting "
-             "Python\\DataFilter\\datas\\inputs\\csv\\students.csv")
+             "Python\\DataFilter\\datas\\inputs\\json\\students.json")
     if not datas:
         exit()
     for data in datas:
